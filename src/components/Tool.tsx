db599d3e58b3ae195443166e865e3803f1a94ec7
Francis Verissimo <francissv97@gmail.com>
2022-04-10 20:27:43 -0400

    add deleteTool function

--- src/components/Tool.tsx
+++ src/components/Tool.tsx
@@ -1,6 +1,8 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
 
+import { refreshPage } from "../App";
+
 import "../styles/tool.scss";
 
 type ToolsType = {
@@ -12,11 +14,29 @@ type ToolsType = {
 };
 
 export function Tool(props: ToolsType) {
+  async function deleteTool(toolId: number, toolTitle: string) {
+    if (window.confirm(`Are you sure you want to remove ${toolTitle}`)) {
+      try {
+        await fetch(`http://localhost:3000/tools/${toolId}`, {
+          method: "DELETE",
+          headers: { "Content-Type": "application/json" }
+        });
+
+        refreshPage();
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  }
+
   return (
-    <div id="tool" key={props.id}>
+    <div
+      id="tool"
+      key={props.id}
+    >
       <div className="headerTool">
         <div className="title">{props.title}</div>
-        <button>
+        <button onClick={() => deleteTool(props.id, props.title)}>
           <FontAwesomeIcon icon={faXmark} /> remove
         </button>
       </div>
