4fc119f085257ed02190e7ec5bef5ca136f50e1f
Francis Verissimo <francissv97@gmail.com>
2022-04-27 01:03:26 -0400

    add loading animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { animated, useTransition } from "react-spring";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Facebook } from "react-content-loader";
 
 import { faPlusCircle } from "@fortawesome/free-solid-svg-icons";
 
@@ -32,7 +33,7 @@ export function refreshPage() {
 
 function App() {
   const [showModal, setShowModal] = useState(false);
-  const [tools, setTools] = useState<ToolsType[]>([]);
+  const [tools, setTools] = useState<ToolsType[]>();
   const [inputSearch, setInputSearch] = useState("");
   const [searchTagsOnly, setSearchTagsOnly] = useState(false);
 
@@ -40,7 +41,7 @@ function App() {
     config: { duration: 250 },
     from: { opacity: 0 },
     enter: { opacity: 1 },
-    leave: { opacity: 0}
+    leave: { opacity: 0 }
   });
 
   function openModalForm() {
@@ -99,66 +100,78 @@ function App() {
         </header>
       </SectionHeader>
       <Main>
-        <SubHeader>
-          <SearchInputs>
-            <Checkbox>
-              <input
-                type="checkbox"
-                name="seachTagsOnly"
-                id="seachTagsOnly"
-                checked={searchTagsOnly}
-                onChange={e => {
-                  setSearchTagsOnly(e.target.checked);
-                }}
-              />
-              <label htmlFor="seachTagsOnly">search in tags only</label>
-            </Checkbox>
-            <input
-              type="text"
-              name="inputTextSeach"
-              id="inputTextSeach"
-              maxLength={25}
-              onChange={e => {
-                setInputSearch(e.target.value);
-              }}
-              value={inputSearch}
-            />
-          </SearchInputs>
-          <ButtonOpenModalForm>
-            <FontAwesomeIcon icon={faPlusCircle} onClick={openModalForm} />
-          </ButtonOpenModalForm>
-        </SubHeader>
-
-        {transtion((style, item) =>
-          item ? (
-            <animated.div style={style}>
-              <AddNewTool
-                onClickCloseButton={() => setShowModal(prev => !prev)}
-                setShowModal={setShowModal}
-                showModal={showModal}
-              />
-            </animated.div>
-          ) : null
-        )}
-
-        <section id="sectionTools">
-          {tools.length > 0 ? (
-            tools.map(tool => {
-              return (
-                <Tool
-                  key={tool.id}
-                  id={tool.id}
-                  title={tool.title}
-                  description={tool.description}
-                  link={tool.link}
-                  tags={tool.tags.map(tag => `#${tag} `)}
+        {tools === undefined ? (
+          <Facebook
+            style={{ paddingTop: "5rem" }}
+            viewBox="0 50 100 100"
+            backgroundColor="#bbb"
+            foregroundColor="#ddd"
+          />
+        ) : (
+          <>
+            <SubHeader>
+              <SearchInputs>
+                <Checkbox>
+                  <input
+                    type="checkbox"
+                    name="seachTagsOnly"
+                    id="seachTagsOnly"
+                    checked={searchTagsOnly}
+                    onChange={e => {
+                      setSearchTagsOnly(e.target.checked);
+                    }}
+                  />
+                  <label htmlFor="seachTagsOnly">search in tags only</label>
+                </Checkbox>
+                <input
+                  type="text"
+                  name="inputTextSeach"
+                  id="inputTextSeach"
+                  maxLength={25}
+                  onChange={e => {
+                    setInputSearch(e.target.value);
+                  }}
+                  value={inputSearch}
                 />
-              );
-            })
-          ) : (
-            <SearchNoResult keyWords={inputSearch} />
-          )}
-        </section>
+              </SearchInputs>
+              <ButtonOpenModalForm>
+                <FontAwesomeIcon icon={faPlusCircle} onClick={openModalForm} />
+              </ButtonOpenModalForm>
+            </SubHeader>
+
+            {transtion((style, item) =>
+              item ? (
+                <animated.div style={style}>
+                  <AddNewTool
+                    onClickCloseButton={() => setShowModal(prev => !prev)}
+                    setShowModal={setShowModal}
+                    showModal={showModal}
+                  />
+                </animated.div>
+              ) : null
+            )}
+
+            <section id="sectionTools">
+              {tools &&
+                tools.length > 0 &&
+                tools.map(tool => {
+                  return (
+                    <Tool
+                      key={tool.id}
+                      id={tool.id}
+                      title={tool.title}
+                      description={tool.description}
+                      link={tool.link}
+                      tags={tool.tags.map(tag => `#${tag} `)}
+                    />
+                  );
+                })}
+              {tools && tools.length === 0 && (
+                <SearchNoResult keyWords={inputSearch} />
+              )}
+            </section>
+          </>
+        )}
       </Main>
     </>
   );
